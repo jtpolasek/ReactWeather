86f7bd16525570fbf25f4a52617afd7434a5cf2a
Todd <tpolasek@gmail.com>
2016-12-03 13:14:24 -0800

    add foundation and jquery

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,8 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About Component!</h3>
-      <p>Welcome to the about page</p>
+      <h3>About</h3>
+      <p>Welcome to the about page.</p>
     </div>
   )
 };
