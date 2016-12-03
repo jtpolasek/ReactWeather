f2a6670c5ed5651dd9dba5d05fac4c29f74eb957
Todd <tpolasek@gmail.com>
2016-12-03 15:44:56 -0800

    Added foundation grid to main and examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return(
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locactions to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Danville'>Danville, CA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Honolulu'>Honolulu, HI</Link>
+        </li>
+      </ol>
     </div>
   )
 }
